ec7cdd8c47ed6be6d1c9744f6ed15f45ab3e7ce2
Alfian <alfian.org@gmail.com>
2022-06-12 19:19:43 +0700

    10 - Challenge Advanced Callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,24 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  arrayFile = [file1, file2, file3];
+  
+  let result = [];
+  arrayFile.forEach((file, index) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) throw err;
+      JSON.parse(data, (key, value) => {
+        if (key === 'message') {
+          result.push(value.split(' ')[1]);
+        }
+      });
+      if (index === arrayFile.length - 1) {
+        fnCallback(err, result);
+      }
+    })
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
